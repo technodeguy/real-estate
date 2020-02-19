9c54acc5c735f15d90f167f5cc7bc6b915dad514
Node <yuriy@innovaticx.com>
2020-02-19 16:18:33 +0200

    Created getAllUsers controller and model methods

--- api/controllers/user_controller.go
+++ api/controllers/user_controller.go
@@ -3,9 +3,14 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/technodeguy/real-estate/api/models"
 	"github.com/technodeguy/real-estate/api/responses"
 )
 
 func (server *Server) GetUsers(w http.ResponseWriter, request *http.Request) {
-	responses.Json(w, http.StatusOK, map[string]string{"usersCount": "11"})
+	user := models.User{}
+
+	users, _ := user.FindAllUsers(server.db)
+
+	responses.Json(w, http.StatusOK, users)
 }
--- api/models/user.go
+++ api/models/user.go
@@ -1,1 +1,45 @@
 package models
+
+import (
+	"database/sql"
+	"log"
+)
+
+type User struct {
+	Id          int    `json:"id"`
+	Nickname    string `json:"nickname"`
+	PhoneNumber string `json:"phoneNumber"`
+	// Avatar      string `json:"avatar"`
+}
+
+func (u *User) FindAllUsers(db *sql.DB) ([]User, error) {
+
+	rows, err := db.Query("SELECT id, nickname, phone_number FROM user")
+
+	if err != nil {
+		log.Println("Error while retrieving all users")
+		return nil, err
+	}
+
+	// var users []User
+	users := make([]User, 0)
+
+	for rows.Next() {
+		user := User{}
+
+		err = rows.Scan(&user.Id, &user.Nickname, &user.PhoneNumber)
+
+		if err != nil {
+			log.Println("Error by parsing - findAllUsers")
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
